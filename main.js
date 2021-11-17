9555bcb03b255d166f89763743388f1767a01c27
Thomas MALACLET <94492568+Freddysenterie@users.noreply.github.com>
2021-11-17 16:48:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,23 @@
+const { Ellipse, Color } = require("scenegraph");
 const commands = require("commands");
-const { Rectangle, DrawEllipse, Color, TextStyle } = require("scenegraph");
 
-function MyCreateRectangle(selection) {
+function ellipse(selection) {
     console.log(selection.items);
-    const elem = new Rectangle();
-    elem.width = 100;
-    elem.height = 60;
-    elem.fill = new Color("Purple");
+    const ellipse = new Ellipse();
+    ellipse.radiusX = 20;
+    ellipse.radiusY = 20;
+    ellipse.width = 200;
+    ellipse.height = 200;
+    ellipse.fill = new Color("red");
+    ellipse.stroke = new Color("blue");
+    
+    selection.insertionParent.addChild (ellipse);
+    ellipse.moveInParentCoordinates(100, 100);
 
-    selection.insertionParent.addChild (elem);
-    elem.moveInParentCoordinates(100, 100);
-
-}
-
-function CreateSquare(selection) {
-    console.log(selection.items);
-    selection.items[0].height = selection.items[0].width;
-}
-
-function TextStyle(selection) {
-    console.log(selection.items);
-    selection.items[0].fontSize = 30;
-    selection.items[0].fill = new Color("Blue");
-
-}
-
-function Ellipse(selection) {
-    console.log(selection.items);
-    const elem = new Ellipse();
-    elem.width = 100;
-    elem.height = 100;
-    elem.stroke = 15
-
-    selection.insertionParent.addChild (elem);
-    elem.moveInParentCoordinates(100, 100);
 }
 
 module.exports = {
     commands: {
-        MyCreateRectangle:MyCreateRectangle,
-        CreateSquare:CreateSquare,
-        TextStyle:TextStyle,
-        DrawEllipse:DrawEllipse
+        ellipse
     }
-} 
+}
