65104a4510f2254afe15d4f98fa84434fec541a4
Thomas MALACLET <94492568+Freddysenterie@users.noreply.github.com>
2021-11-17 14:36:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 const commands = require("commands");
+const { Rectangle, DrawEllipse, Color, TextStyle } = require("scenegraph");
 
-function MyCreateRectangle() {
-    console.log("***TEST***");
-    
+function MyCreateRectangle(selection) {
+    console.log(selection.items);
+    const elem = new Rectangle();
+    elem.width = 100;
+    elem.height = 60;
+    elem.fill = new Color("Purple");
+
+    selection.insertionParent.addChild (elem);
+    elem.moveInParentCoordinates(100, 100);
+
+}
+
+function CreateSquare(selection) {
+    console.log(selection.items);
+    selection.items[0].height = selection.items[0].width;
+}
+
+function TextStyle(selection) {
+    console.log(selection.items);
+    selection.items[0].fontSize = 30;
+    selection.items[0].fill = new Color("Blue");
+
+}
+
+function Ellipse(selection) {
+    console.log(selection.items);
+    const elem = new Ellipse();
+    elem.width = 100;
+    elem.height = 100;
+    elem.stroke = 15
+
+    selection.insertionParent.addChild (elem);
+    elem.moveInParentCoordinates(100, 100);
 }
 
 module.exports = {
     commands: {
-        MyCreateRectangle
+        MyCreateRectangle:MyCreateRectangle,
+        CreateSquare:CreateSquare,
+        TextStyle:TextStyle,
+        DrawEllipse:DrawEllipse
     }
 } 
